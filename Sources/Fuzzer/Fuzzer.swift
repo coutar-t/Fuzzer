90a8d94a0ab5ef1ca79349a0a73fd671e9f1b37f
Thibaut Coutard <thibaut@stamp.yt>
2021-11-19 18:05:56 +0100

    Initial Commit

---
+++ Sources/Fuzzer/Fuzzer.swift
@@ -0,0 +1,6 @@
+public struct Fuzzer {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FuzzerTests/FuzzerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Fuzzer
+
+final class FuzzerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Fuzzer().text, "Hello, World!")
+    }
+}
